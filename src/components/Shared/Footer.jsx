ea09d00da5c718f9eeaf9d7d80b84d0e2a09da59
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-04-14 14:23:04 +0600

    Footer section designed

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -17,14 +17,30 @@ const Footer = () => {
           </p>
           <h4 className="font-medium text-xl">Social Links</h4>
           <div className="mt-4 flex items-center justify-center gap-3">
-            <div className="bg-white text-black rounded-full p-2.5 inline-block cursor-pointer">
+            <div className="bg-white text-black hover:text-blue-500 transition-all duration-300 ease-in-out rounded-full p-2.5 inline-block cursor-pointer">
               <FaInstagram size={20} />
             </div>
-            <div className="bg-white text-black rounded-full p-2.5 inline-block cursor-pointer">
+            <div className="bg-white text-black hover:text-blue-500 transition-all duration-300 ease-in-out rounded-full p-2.5 inline-block cursor-pointer">
               <FaFacebookSquare size={20} />
             </div>
-            <div className="bg-white text-black rounded-full p-2.5 inline-block cursor-pointer">
+            <div className="bg-white text-black hover:text-blue-500 transition-all duration-300 ease-in-out rounded-full p-2.5 inline-block cursor-pointer">
               <FaXTwitter size={20} />
+            </div>
+          </div>
+          <div className="flex flex-wrap gap-4 items-center justify-center sm:justify-between text-white/50 border-t border-[#1A8862] mt-10 pt-7.5">
+            <div>
+              <p>© 2026 KeenKeeper. All rights reserved.</p>
+            </div>
+            <div className="flex items-center gap-4">
+              <p className="cursor-pointer hover:underline hover:text-white transition-all duration-300 ease-in-out">
+                Privacy Policy{" "}
+              </p>
+              <p className="cursor-pointer hover:underline hover:text-white transition-all duration-300 ease-in-out">
+                Terms of Service{" "}
+              </p>
+              <p className="cursor-pointer hover:underline hover:text-white transition-all duration-300 ease-in-out">
+                Cookies
+              </p>
             </div>
           </div>
         </div>
