710cf238bf5c34f8084022fc8db5617be171b78a
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-04-16 00:30:43 +0600

    Stats page designed

--- src/app/stats/page.jsx
+++ src/app/stats/page.jsx
@@ -3,8 +3,14 @@ import MyContainer from "@/components/Shared/MyContainer";
 import NoData from "@/components/Shared/NoData";
 import { MyContext } from "@/context/TimelineContext";
 import React, { useContext } from "react";
+import { Legend, Pie, PieChart, Tooltip } from "recharts";
 
 const StatsPage = () => {
+  const data = [
+    { name: "Text", value: 300, fill: "#FFBB28" },
+    { name: "Call", value: 200, fill: "#FF8042" },
+    { name: "Video", value: 400, fill: "#0088FE" },
+  ];
   const { timelines } = useContext(MyContext);
   console.log(timelines);
   return (
@@ -15,7 +21,37 @@ const StatsPage = () => {
         </h3>
         {timelines.length > 0 ? (
           <>
-            <p>Data</p>
+            <div className="bg-white mt-6 p-8 rounded-xl shadow">
+              <h4 className="font-medium text-[#244D3F] text-xl mb-6">
+                By Interaction Type
+              </h4>
+              <div className="flex items-center justify-center">
+                <PieChart
+                  style={{
+                    width: "100%",
+                    maxWidth: "500px",
+                    maxHeight: "80vh",
+                    aspectRatio: 1,
+                  }}
+                  responsive
+                >
+                  <Pie
+                    data={data}
+                    innerRadius="80%"
+                    outerRadius="100%"
+                    // Corner radius is the rounded edge of each pie slice
+                    cornerRadius="50%"
+                    fill="#8884d8"
+                    // padding angle is the gap between each pie slice
+                    paddingAngle={5}
+                    dataKey="value"
+                    isAnimationActive={true}
+                  />
+                  <Legend />
+                  <Tooltip />
+                </PieChart>
+              </div>
+            </div>
           </>
         ) : (
           <>
